e5bd708678708d65062d54723d01f266cbc6f19b
Tayyab Khan <tayyabkhangk4734@gmail.com>
2025-03-20 01:31:50 +0530

    vercel-fix

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Eye, EyeOff, Link } from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
 import "../src/app/globals.css";
 
 export default function Signup() {
